afdbeea4284b0e4c7efc2ce22a275fdcb4670f62
Keagan Simpson <keagan8simpson@gmail.com>
2024-10-07 21:05:57 +0200

    version 1

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in C#, JavaScript, CSS and HTML.
+    I've just started my journey in Game dev using Godot. Passionate about programming and always trying to learn more.
+    Here is my <a href="https://github.com/SimplyLearning" target="_blank">Github</a>!`,
+  "cs-degree": `Under Construction`,
+  "sofa-table": `This is my relaxation spot. I heard that resting after the gym is optimal (Bro Science).`,
+  tv: `That's my TV. I watch youtubers such as:
+   <a href="https://www.youtube.com/@developedbyed" target="_blank">Developedbyd</a>, <a href="https://www.youtube.com/@LinusTechTips" target="_blank">LinusTechTips</a>,
+   and <a href="https://www.youtube.com/@Jayztwocents" target="_blank">JayzTwoCents</a>!`,
+  bed: `This where I sleep and most times brainstorm 100 different ways I could code something!`,
+  resume: `This is my resume. <a href="./CV.html" target="_blank">Check it out?</a>
+  Contact me at keagan8simpson@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  library: `This is where I store all documents that have come in handy including all the things I have learnt if i need a refresher`,
+  exit: `Don't leave just yet, make sure you look around!`,
+};
